a775a214e97041964da60368d33761726cc28751
Sethen Maleno <sethenm@gmail.com>
2018-07-12 02:08:58 -0400

    Put repos script back

--- run.sh
+++ run.sh
@@ -9,3 +9,4 @@ export ZSH_DIRECTORY_PATH=${0:a:h}
 "${ZSH_DIRECTORY_PATH}/${OS}/bootstrap.sh"
 "${ZSH_DIRECTORY_PATH}/${OS}/setup.sh"
 "${ZSH_DIRECTORY_PATH}/${OS}/preferences.sh"
+"${ZSH_DIRECTORY_PATH}/repos.sh"
