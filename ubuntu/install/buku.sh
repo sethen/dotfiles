7333f398cf8b17957c2cc2ad0d25d4b01906b823
Sethen Maleno <sethenm@gmail.com>
2020-06-08 17:57:40 -0400

    Fixed buku install and tmux

--- ubuntu/install/buku.sh
+++ ubuntu/install/buku.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
-pip3 install buku
+if ! type buku > /dev/null; then
+	information_message "installing buku"
+
+	pip3 install buku
+else
+	success_message "buku already installed"
+fi
--- ubuntu/install/tmux.sh
+++ ubuntu/install/tmux.sh
@@ -1,12 +1,3 @@
 #!/bin/zsh
 
-if ! type tmux > /dev/null; then
-	information_message 'installing tmux'
-
-	TMUX=/usr/bin/tmux
-
-	sudo wget -O $TMUX https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}a/tmux-${TMUX_VERSION}a-x86_64.AppImage
-	sudo chmod +x $TMUX
-else
-	success_message 'tmux already installed'
-fi
+apt_get_install_if_package_not_exists tmux
