ede7aef5bb3c9945b13a13eda27f60afee4b329d
Sethen Maleno <sethenm@gmail.com>
2021-04-01 17:06:33 -0400

    Fixed issues

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -60,7 +60,6 @@ check_if_file_exists_executable $PRESENT_WORKING_DIRECTORY/ubuntu/install/jdk.sh
 
 # nodejs install order matters here
 check_if_file_exists_executable $PRESENT_WORKING_DIRECTORY/ubuntu/install/nodejs.sh
-check_if_file_exists_executable $PRESENT_WORKING_DIRECTORY/ubuntu/install/npm.sh
 check_if_file_exists_executable $PRESENT_WORKING_DIRECTORY/ubuntu/install/neovim-node-host.sh
 
 # python3 install order matters here
