ce70c8d0eab6e89467f50831ae4d1825bb2016f5
Sethen Maleno <sethen@github.com>
2022-09-12 19:07:34 -0400

    Changed company completion with meta-l

--- os/.emacs.d/lisp/packages/sethen-company.el
+++ os/.emacs.d/lisp/packages/sethen-company.el
@@ -31,10 +31,10 @@
   :bind (:map company-active-map
 			  ("M-j" . 'company-select-next)
 			  ("M-k" . 'company-select-previous)
-			  ("M-l" . 'company-complete)
+			  ("M-l" . 'company-complete-selection)
 			  ("M-n" . 'company-other-backend))
   :commands
-  (company-complete company-other-backend company-select-next company-select-previous global-company-mode)
+  (company-complete-selection company-other-backend company-select-next company-select-previous global-company-mode)
   :config
   (setq company-idle-delay 0
 		company-minimum-prefix-length 1)
