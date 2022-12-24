c7a4665a7c80918bb853fadfd1271f8963e64b25
Sethen Maleno <sethen@github.com>
2022-12-24 15:12:42 -0500

    Added dependencies for emacs native compile

--- ubuntu/setup/packages/emacs.sh
+++ ubuntu/setup/packages/emacs.sh
@@ -6,7 +6,7 @@ else
 	information_message "installing emacs"
 
 	sudo apt build-dep emacs
-	sudo apt install gcc-12 -y
+	sudo apt install gcc-12 libgccjit0 libgccjit-12-dev -y
 
 	wget -O ~/$EMACS_DOWNLOAD_DIRECTORY.tar.gz http://mirror.keystealth.org/gnu/emacs/$EMACS_DOWNLOAD_DIRECTORY.tar.gz
 	tar -xvf ~/$EMACS_DOWNLOAD_DIRECTORY.tar.gz -C ~
