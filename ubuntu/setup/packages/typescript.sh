15c0453619d7e0e7b6ec0d2ab9a919e84c997b68
Sethen Maleno <sethen@github.com>
2023-01-11 12:20:46 -0500

    Added typescript language server to npm

--- ubuntu/setup/packages/typescript.sh
+++ ubuntu/setup/packages/typescript.sh
@@ -4,7 +4,7 @@ if [[ -d $NPM_PACKAGES_DIRECTORY ]]; then
 	if [[ ! -a $NPM_PACKAGES_BIN_DIRECTORY/tsc ]]; then
 		information_message 'installing typescript'
 
-		npm install -g typescript
+		npm install -g typescript typescript-language-server
 	else
 		success_message 'typescript already installed'
 	fi
