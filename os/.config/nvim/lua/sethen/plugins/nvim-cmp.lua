3891bdd5568251e4b1841f8a4456afe6c16e7a59
Sethen Maleno <sethen@github.com>
2024-01-02 16:37:23 -0500

    Changed fonts changed mappings for nvim-cmp

--- os/.config/nvim/lua/sethen/plugins/nvim-cmp.lua
+++ os/.config/nvim/lua/sethen/plugins/nvim-cmp.lua
@@ -1,5 +1,6 @@
 return {
 	"hrsh7th/nvim-cmp",
+	event = { "InsertEnter", "CmdlineEnter" },
 	dependencies = {
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
@@ -7,15 +8,53 @@ return {
 	config = function()
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
 
 		cmp.setup({
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				["<C-j>"] = cmp.mapping.select_next_item(),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-x>"] = cmp.mapping.abort(),
-				["<C-l>"] = cmp.mapping.confirm({ select = false }),
-			}),
+			mapping = {
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						if #cmp.get_entries() == 1 then
+							cmp.confirm({ select = true })
+						else
+							cmp.select_next_item()
+						end
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+						if #cmp.get_entries() == 1 then
+							cmp.confirm({ select = true })
+						end
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<CR>"] = cmp.mapping({
+					i = function(fallback)
+						if cmp.visible() and cmp.get_active_entry() then
+							cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+						else
+							fallback()
+						end
+					end,
+					s = cmp.mapping.confirm({ select = true }),
+					c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+				}),
+			},
 			sources = {
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
