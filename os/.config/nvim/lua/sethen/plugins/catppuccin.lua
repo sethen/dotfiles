32502428d1b2b2c8aa47c0a905a455c5d3e6ba3a
Sethen Maleno <sethen@github.com>
2024-01-04 23:01:09 -0500

    CursorLineNr change in neovim

--- os/.config/nvim/lua/sethen/plugins/catppuccin.lua
+++ os/.config/nvim/lua/sethen/plugins/catppuccin.lua
@@ -4,8 +4,10 @@ return {
       require("catppuccin").setup({
          flavour = "mocha",
       });
+      local palette = require("catppuccin.palettes").get_palette("mocha")
 
       vim.cmd.colorscheme "catppuccin"
+      vim.api.nvim_set_hl(0, "CursorLineNr", {fg=palette.lavender, bg=palette.mantle, bold = true})
    end,
    name = "catppuccin",
    priority = 1000
