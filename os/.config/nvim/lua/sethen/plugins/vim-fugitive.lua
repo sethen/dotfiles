48d8c3fe5f04982ec60512fc2c5fcacf73617d35
Sethen Maleno <sethen@github.com>
2024-04-01 00:29:52 -0400

    Changed fugitive keymaps

--- os/.config/nvim/lua/sethen/plugins/vim-fugitive.lua
+++ os/.config/nvim/lua/sethen/plugins/vim-fugitive.lua
@@ -2,10 +2,8 @@ return {
    "tpope/vim-fugitive",
    opt = true,
    keys = {
-      { "<Space>ga", "<cmd>Git add<CR>", desc = "Git add" },
       { "<Space>gc", "<cmd>Git commit<CR>", desc = "Git commit" },
       { "<Space>gi", "<cmd>Git<CR>", desc = "Git interface" },
       { "<Space>gp", "<cmd>Git push<CR>", desc = "Git push" },
-      { "<Space>gs", "<cmd>Git stage<CR>", desc = "Git stage" },
    },
 }
