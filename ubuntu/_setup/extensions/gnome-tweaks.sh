4834ad47703ee50483b8b0e0e1fb9a1311d979ec
Sethen Maleno <sethen@github.com>
2024-05-01 17:53:51 -0400

    Gnome dark mode and theme add

--- ubuntu/_setup/extensions/gnome-tweaks.sh
+++ ubuntu/_setup/extensions/gnome-tweaks.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
+dconf write /org/gnome/desktop/interface/gtk-theme "'Yaru-blue-dark'"
+dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
 dconf write /org/gnome/desktop/interface/icon-theme "'Colloid-dark'"
--- ubuntu/_setup/packages/colloid-icon-theme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-if [[ -z $(dconf read /org/gnome/desktop/interface/icon-theme | grep "'Colloid-dark'") ]]; then
-    information-message 'colloid icons not installed'
-else
-    success-message 'colloid icons installed'
-fi
