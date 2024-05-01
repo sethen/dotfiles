e04b0ce508b4294b35a5f0a740f4bd70cad26eb0
Sethen Maleno <sethen@github.com>
2024-05-01 09:41:18 -0400

    Options and dash to dock changes

--- ubuntu/_setup/extensions/dash-to-dock.sh
+++ ubuntu/_setup/extensions/dash-to-dock.sh
@@ -3,6 +3,6 @@
 dconf write /org/gnome/shell/extensions/dash-to-dock/dash-max-icon-size 48
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed false
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'BOTTOM'"
-dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.Calculator.desktop', 'gpick.desktop', 'org.gnome.Calendar.desktop', 'gparted.desktop', 'google-chrome.desktop', 'kitty.desktop', 'transmission-gtk.desktop', 'vlc.desktop', 'org.gnome.Settings.desktop', org.gnome.tweaks.desktop']"
+dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.Calculator.desktop', 'gpick.desktop', 'org.gnome.Calendar.desktop', 'gparted.desktop', 'google-chrome.desktop', 'kitty.desktop', 'transmission-gtk.desktop', 'vlc.desktop', 'org.gnome.Settings.desktop', 'org.gnome.tweaks.desktop']"
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-mounts false
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-trash false
