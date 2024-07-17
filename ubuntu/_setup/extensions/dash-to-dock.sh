79f66465f5af59d030005685494d6d7efb575ae1
Sethen Maleno <sethen@github.com>
2024-07-17 19:00:07 -0400

    Fixed typo in dash to dock

--- ubuntu/_setup/extensions/dash-to-dock.sh
+++ ubuntu/_setup/extensions/dash-to-dock.sh
@@ -6,5 +6,5 @@ dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'BOTTOM'"
 dconf write /org/gnome/shell/extensions/dash-to-dock/extend-height false
 dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.Calculator.desktop', 'gpick.desktop', 'gparted.desktop', 'google-chrome.desktop', 'kitty.desktop', 'vlc.desktop', 'org.gnome.Settings.desktop', 'org.gnome.tweaks.desktop']"
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-mounts false
-dconf watch /org/gnome/shell/extensions/dash-to-dock/show-show-apps-button false
+dconf write /org/gnome/shell/extensions/dash-to-dock/show-show-apps-button false
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-trash false
