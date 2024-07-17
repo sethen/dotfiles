c214cf3344ce4c6bb3c771d3ebdbe93296f0dcec
Sethen Maleno <sethen@github.com>
2024-07-17 18:40:30 -0400

    Dash to dock changes

--- ubuntu/_setup/extensions/dash-to-dock.sh
+++ ubuntu/_setup/extensions/dash-to-dock.sh
@@ -3,6 +3,8 @@
 dconf write /org/gnome/shell/extensions/dash-to-dock/dash-max-icon-size 48
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed false
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'BOTTOM'"
+dconf write /org/gnome/shell/extensions/dash-to-dock/extend-height false
 dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.Calculator.desktop', 'gpick.desktop', 'gparted.desktop', 'google-chrome.desktop', 'kitty.desktop', 'vlc.desktop', 'org.gnome.Settings.desktop', 'org.gnome.tweaks.desktop']"
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-mounts false
+dconf watch /org/gnome/shell/extensions/dash-to-dock/show-show-apps-button false
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-trash false
