8532e760f8f3cd8f30de45b42b215a9d0264541b
Sethen Maleno <sethen@github.com>
2025-07-05 21:43:15 -0400

    Added brave browser to dock

--- distro/ubuntu/_setup/extensions/dash-to-dock.sh
+++ distro/ubuntu/_setup/extensions/dash-to-dock.sh
@@ -4,7 +4,7 @@ dconf write /org/gnome/shell/extensions/dash-to-dock/dash-max-icon-size 48
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed false
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'BOTTOM'"
 dconf write /org/gnome/shell/extensions/dash-to-dock/extend-height false
-dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.Calculator.desktop', 'gpick.desktop', 'gparted.desktop', 'google-chrome.desktop', 'kitty.desktop', 'vlc.desktop', 'org.gnome.Settings.desktop', 'org.gnome.tweaks.desktop']"
+dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'brave-browser.desktop', 'org.gnome.Calculator.desktop', 'gpick.desktop', 'gparted.desktop', 'google-chrome.desktop', 'kitty.desktop', 'vlc.desktop', 'org.gnome.Settings.desktop', 'org.gnome.tweaks.desktop']"
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-mounts false
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-show-apps-button false
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-trash false
