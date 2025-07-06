4bcb54b16d29b5afe026a41c5d87bc9c0b9da104
Sethen Maleno <sethen@github.com>
2025-07-05 21:38:37 -0400

    Added user to docker group

--- os/_setup/run.sh
+++ os/_setup/run.sh
@@ -3,12 +3,14 @@
 echo ''
 header-message 'os setup'
 
-# if [ $(getent group docker) ]; then
-#    success-message "docker group exists"
-# else
-#    information-message "creating docker group and adding user"
-#
-#    sudo groupadd docker
-#    sudo usermod -aG docker $USER
-#    newgrp docker
-# fi
+if ! getent group docker > /dev/null; then
+  sudo groupadd docker
+fi
+
+if ! id -nG "$USER" | grep -qw docker; then
+  information-message "adding $USER to docker group -- you will need to log out and back in for this to take effect"
+
+  sudo usermod -aG docker "$USER"
+else
+  success-message "$USER already added to docker group"
+fi
