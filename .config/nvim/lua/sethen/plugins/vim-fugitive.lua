47f3381062a4c7809deb7e3362083e76f1a8e586
Sethen Maleno <sethen@github.com>
2025-10-17 00:18:54 -0400

    Small tweaks

--- .config/nvim/lua/sethen/plugins/vim-fugitive.lua
+++ .config/nvim/lua/sethen/plugins/vim-fugitive.lua
@@ -5,7 +5,7 @@ return {
    opt = true,
    keys = {
       { '<Space>gc', '<cmd>Git commit<CR>', desc = 'Git commit' },
-      { '<Space>gi', '<cmd>Git<CR>', desc = 'Git interface' },
-      { '<Space>gp', '<cmd>Git push<CR>', desc = 'Git push' },
+      { '<Space>gi', '<cmd>Git<CR>',        desc = 'Git interface' },
+      { '<Space>gp', '<cmd>Git push<CR>',   desc = 'Git push' },
    },
 }
