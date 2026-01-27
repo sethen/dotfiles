3d1b7de4850a930061fc46b902b11c3b49b5e035
Sethen Maleno <sethen@github.com>
2026-01-27 18:11:51 -0500

    fix ncurses install message

--- os/ubuntu/install/apt/init/install-ncurses.fish
+++ os/ubuntu/install/apt/init/install-ncurses.fish
@@ -1,7 +1,13 @@
 #!/usr/bin/env fish
 
 function install-ncurses
-  running-message "install-ncurses"
+  running-message "installing ncurses"
 
-  sudo-apt-install-package ncurses-dev
+  if not pkg-config --exists ncursesw
+    information-message "installing ncurses"
+
+    sudo apt-get install -y libncursesw5-dev
+  else
+    success-message "ncurses already installed"
+  end
 end
