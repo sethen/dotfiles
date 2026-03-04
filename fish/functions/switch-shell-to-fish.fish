18514308a757f0460ecff081f3527f58f680056e
Sethen Maleno <sethen@github.com>
2026-03-04 15:49:21 -0500

    added lazyssh and fixed fish shell script

--- fish/functions/switch-shell-to-fish.fish
+++ fish/functions/switch-shell-to-fish.fish
@@ -1,13 +1,24 @@
 #!/usr/bin/env fish
 
 function switch-shell-to-fish
-        running-message "switch-shell-to-fish"
+    running-message switch-shell-to-fish
 
-        if test "$SHELL" != "/usr/bin/fish"
+    set -l fish_path (command -v fish)
+
+    if test -z "$fish_path"
+        error-message "fish not found in PATH"
+        return 1
+    end
+
+    if not contains "$fish_path" (cat /etc/shells)
+        echo "$fish_path" | sudo tee -a /etc/shells
+    end
+
+    if test "$SHELL" != "$fish_path"
         information-message "switching shell to fish, you may need to re-login"
 
-        chsh -s /usr/bin/fish
-        else
+        chsh -s "$fish_path"
+    else
         success-message "shell is already fish"
-        end
+    end
 end
--- os/darwin/pre/run-darwin-pre.fish
+++ os/darwin/pre/run-darwin-pre.fish
@@ -3,11 +3,22 @@
 function run-darwin-pre
     running-message run-darwin-pre
 
+    # install
+    install-homebrew
+
+    # update & upgrade
+    if test "$RUN_DOTFILES_UPDATE" = true
+
+        brew update
+        brew upgrade
+    else
+        information-message "run dotfiles update flag not found, skipping update"
+    end
+
     # prep
     copy-fonts
     touch-hushlogin
 
     # install
-    install-homebrew
     install-gh
 end
